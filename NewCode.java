f53e545b8bf443f3cc673a1e1698c68156aa8f53
xuehui-code <74036099+xuehui-code@users.noreply.github.com>
2020-12-30 21:10:08 +0800

    Add files via upload

---
+++ NewCode.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class NewCode {
+    public static void main(String[] args){
+        String in = " ";
+        String out = " ";
+
+        Scanner sc = new Scanner(System.in);
+        String in = sc.next();
+        String out = sc.next();
+
+        //遍历全部字母
+        int i =0;
+        for(i = 0,i<in.length(),i++)
+
+        Set<Character in = new TreeSet<>();
+        Set<Character> out = new TreeSet<>();
+    }
+}
