f6c039381e166aa8ff95ce30659a4e929f10c629
xuehui-code <74036099+xuehui-code@users.noreply.github.com>
2021-03-29 08:32:25 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,64 @@
+package nowcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class test {
+ /*   public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int n = Integer.parseInt(s);
+        while(n<1){
+            System.out.println(n%10);
+            n = n/10;
+            if(n == 0){
+                break;
+            }
+        }
+    }*/
+ /*public static void main(String[] args)throws IOException {
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     String s = br.readLine();
+     int n = Integer.parseInt(s);
+     while(true){
+         System.out.print(n%10);
+         n = n / 10;
+         if(n == 0){
+             break;
+         }
+     }
+ }*/
+/* public static void main(String[] args)throws IOException{
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     String s = br.readLine();
+     int n = Integer.parseInt(s);
+     double x = 3.156E7;
+     System.out.println((double)n*x);
+ }*/
+/*    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String x = br.readLine();
+        long n = Integer.parseInt(x);
+        long h = n/3600;
+        long m = (n-3600*h)/60;
+        long s = n%60;
+        System.out.println((int)h+"  "+(int)m+"  "+(int)s);
+    }*/
+
+/*    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        double a = Double.parseDouble(s[0]);
+        double b = Double.parseDouble(s[1]);
+        double c = Double.parseDouble(s[2]);
+        double sum = a+b+c;
+        double n = sum/3;
+        System.out.println(sum+" "+String.format("%.2f",n));
+        return ;
+    }*/
+
+/*    public static void main(String[] args)  {
+        System.out.println("I lost my cellphone");
+    }*/
+}
