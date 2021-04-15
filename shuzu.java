524159f0382072e0fc0cf6a14040127647e76532
xuehui-code <74036099+xuehui-code@users.noreply.github.com>
2021-04-15 21:20:03 +0800

    Add files via upload

---
+++ shuzu.java
@@ -0,0 +1,11 @@
+package disizhang;
+
+public class shuzu {
+    public static void main(String[] args){
+        int[] arr = new int[20];
+        for(int i=0;i<20;i++){
+            arr[i] = i;
+        }
+        System.out.println(arr[5]);
+    }
+}
