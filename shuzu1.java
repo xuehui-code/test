db683d27e36dcb86f503fff230e065b3370507bd
xuehui-code <74036099+xuehui-code@users.noreply.github.com>
2021-06-01 21:13:26 +0800

    Add files via upload

---
+++ shuzu1.java
@@ -0,0 +1,10 @@
+public class shuzu1 {
+    public static void main(String[] args){
+        int[] A;
+        A = new int[]{2, 4, 5, 7, 9};
+        for(int i=0;i<5;i++){
+            System.out.println(A[i]);
+        }
+
+    }
+}
