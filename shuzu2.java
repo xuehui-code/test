621b5eb39b2c168657400799afc4a9e9f7637b1c
xuehui-code <74036099+xuehui-code@users.noreply.github.com>
2021-06-03 15:16:22 +0800

    Add files via upload

---
+++ shuzu2.java
@@ -0,0 +1,11 @@
+public class shuzu2 {
+    public static void main(String[] args) {
+        int[][]  arr ={{15,17,19},{12,14,16},{25,36,47},{22,65,15},{22,34,23}};
+        for(int i=0;i<5;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
